91e4a1ec01c043bcfac7821fd4c962c7e3c0c831
zhangb-top <2629739216@qq.com>
2023-06-24 12:51:51 +0800

    完善二次提取图片

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', function () {
   const request = axios.create({
     baseURL: 'http://www.zhangb.top:8111',
-  });
+  })
 
+  // 图片id
+  let id = ''
   // 请求数据
   const data = {
     // P表示第一次生成图片，U表示修改生成过的图片
@@ -23,33 +25,71 @@ window.addEventListener('load', function () {
   // 提交事件
   document.querySelector('.submit').addEventListener('click', makeImg)
 
+  // 底部选择图片按钮点击事件
+  const btnImgChecks = document.querySelectorAll('.btn-img-check')
+  btnImgChecks.forEach((item, index) => {
+    item.addEventListener('click', function () {
+      // 再次点击去除激活状态
+      if (this.className.includes('active')) {
+        this.classList.remove('active')
+        data.type = 'P'
+        data.imgId = ''
+        data.num = 0
+        return
+      }
+      if (document.querySelector('.btn-img-check.active')) {
+        document.querySelector('.btn-img-check.active').classList.remove('active')
+      }
+      this.classList.add('active')
+      data.type = 'U'
+      data.imgId = id
+      data.num = index + 1
+    })
+  })
+
   // 生成图片
   async function makeImg() {
-    if (data.prompt.trim() === '') {
+    if (data.prompt.trim() === '' && data.type === 'P') {
       showTip('提示词不能为空')
       data.prompt = ''
       document.querySelector('.prompt').value = ''
       return
+    } else if (data.prompt.trim() === '' && data.type === 'U') {
+      data.prompt = `提取${new Date().getMilliseconds()}`
     }
     const { data: res } = await request.post('/ai_draw/make', data)
-    if (res.code === 0) return showTip('试试修改部分提示词')
+    if (res.code !== 200) return showTip('试试换个提示词吧')
     const wait = document.querySelector('.wait')
     const submit = document.querySelector('.submit')
     const prompt = document.querySelector('.prompt')
-    wait.style.display = 'block'
+    const image = document.querySelector('.image')
+    const download = document.querySelector('.download-a')
+    const btnImgCheckBox = document.querySelector('.btn-img-check-box')
+    image.src = ''
+    download.href = ''
+    fadeOut(wait)
     submit.disabled = true
     prompt.disabled = true
     prompt.placeholder = '图片加载中禁止输入'
     data.prompt = ''
     document.querySelector('.prompt').value = ''
-    // 等待1分钟再获取图片
+    // 等待30秒再获取图片
     let num = 60
     let timer = setInterval(() => {
       num--
       wait.innerHTML = `${num}秒后获取图片`
       if (num === 0) {
         clearInterval(timer)
-        wait.style.display = 'none'
+        fadeIn(wait)
+        if (data.type === 'U') {
+          fadeIn(btnImgCheckBox)
+          document.querySelector('.btn-img-check.active').classList.remove('active')
+          data.imgId = ''
+          data.num = 0
+          data.type = 'P'
+        } else {
+          fadeOut(btnImgCheckBox)
+        }
         submit.disabled = false
         prompt.disabled = false
         prompt.placeholder = '请输入提示词'
@@ -61,19 +101,32 @@ window.addEventListener('load', function () {
   // 获取图片
   async function getImg(imgId) {
     const { data: res } = await request.post('/ai_draw/get', imgId)
+    id = res.data.imgId
     const image = document.querySelector('.image')
+    const download = document.querySelector('.download-a')
     image.src = `http://midjourney-api.ai-des.com${res.data.url}`
+    download.href = `http://midjourney-api.ai-des.com${res.data.url}`
   }
 
   // 展示提示
   function showTip(message) {
     const tip = document.querySelector('.tip')
     tip.innerHTML = message
-    tip.classList.remove('hide')
-    tip.classList.add('show')
+    fadeOut(tip)
     setTimeout(() => {
-      tip.classList.remove('show')
-      tip.classList.add('hide')
+      fadeIn(tip)
     }, 2000)
+  }
+
+  // 淡出动画
+  function fadeOut(element) {
+    element.classList.remove('hide')
+    element.classList.add('show')
+  }
+
+  // 淡入动画
+  function fadeIn(element) {
+    element.classList.remove('show')
+    element.classList.add('hide')
   }
 })
